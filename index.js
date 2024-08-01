6b4d21698da748630ea5da0bb258667de6751305
Poornima-Ranguri <poornimaranguri@gmail.com>
2024-08-01 13:30:42 +0000

    Congrats card Using React JS

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  <div className="home">
+    <h1>Congratulations</h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="heading">Kiran V</h1>
+      <p className="parag">
+        Vishnu Institution of Computer Education and Technology <br />
+        Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
